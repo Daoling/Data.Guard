1f937f8b2312904ccc0f328452022acdc6bad333
Daoling <daoling.a.song@accenture.com>
2015-09-14 13:32:42 +0800

    Changed to factory pattern

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-var config=require('./config.json');
-var step1= require('./lib/Connect.js');
-var step2= require('./lib/Auth2.js');
-var step3= require('./lib/JsonKey.js');
+var args = process.argv.splice(2);
+var config = require(args[0]);
+var run = require('./lib/run.js');
 
-
-var Connect = new step1.Connect();
-var Auth2 = new step2.Auth2();
-var JsonKey = new step3.JsonKey();
-
-
+run.main(config);
 
